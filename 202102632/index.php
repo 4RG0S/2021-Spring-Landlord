7bb70356a0869c73b2f41bc16dee8133b1facf8f
purplepig <purplepig4657@gmail.com>
2021-04-24 15:44:06 +0900

    add index.php

---
+++ 202102632/index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <title>title</title>
+    </head>
+
+    <body>
+        <form action="index.php" method="post">
+            <input type="number" name="a">
+            <select name="operator" id="operator">
+                <option value="plus">+</option>
+                <option value="subtract">-</option>
+                <option value="multiply">*</option>
+                <option value="division">/</option>
+            </select>
+            <input type="number" name="b">
+            <br>
+            <input type="submit" value="submit">
+        </form>
+    </body>
+</html>
+
+<?PHP
+$A = $_POST["a"];
+$B = $_POST["b"];
+$op = $_POST["operator"];
+
+echo "$A ";
+echo "$op ";
+echo "$B <br>";
+
+switch($op) {
+    case "plus":
+        echo $A + $B;
+        break;
+    case "subtract":
+        echo $A - $B;
+        break;
+    case "multiply":
+        echo $A * $B;
+        break;
+    case "division":
+        echo $A / $B;
+        break;
+}
+?>
