f9d6433d5250fb1e529222e2cbf10a3654748a21
Kangjun Heo == P.Knowledge <knowledge@o.cnu.ac.kr>
2021-05-15 16:16:00 +0900

    연결체크용 스크립트

---
+++ 201704150/db_check.php
@@ -0,0 +1,29 @@
+<?
+    $username = isset($_POST['student_no']) ? $_POST['student_no'] : NULL;
+    $password = isset($_POST['student_pw']) ? $_POST['student_pw'] : NULL;
+
+    if ($username && preg_match("/[0-9]{9}/", $username)) {
+        $host = 'localhost';
+        $id = $_POST['student_no'];
+        $pw = $_POST['student_pw'];
+        $db = "db_" . $_POST['student_no'];
+
+        $dsn = "mysql:host=$host,dbname=$dbname;charset=utf8";
+
+        try {
+            $conn = new PDO($dsn, $id, $pw, [
+                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+            ]);
+
+            echo "연결 성공!<br />";
+        } catch (Exception $e) {
+            echo "연결 실패...<br />";
+        }
+    }
+?>
+
+<form method="post">
+    <input type="text" name="student_no" placeholder="학번" />
+    <input type="password" name="student_pw" placeholder="비밀번호" />
+    <input type="submit" value="테스트" />
+</form>
