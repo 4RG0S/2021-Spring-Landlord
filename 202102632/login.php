6491a000f49c9b61829763d3187561ca325b5111
purplepig <purplepig4657@gmail.com>
2021-05-22 00:55:02 +0900

    db......

--- 202102632/login.php
+++ 202102632/login.php
@@ -1,4 +1,20 @@
 <?php
+    $host = "localhost";
+    $username = "202102632";
+    $password = "1234";
+    $dbname = "db_202102632";
+
+    $dsn = "mysql:dbname=$dbname;host=$host;charset=utf8";
+    $conn = new PDO($dsn, $username, $password);
+
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO_ERRMODE_EXCEPTION);
+
+    $stmt = $conn->prepare("SELECT * FROM Woorizip_Users");
+    $stmt->execute();
+
+    $data = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    echo $data;
+
     session_start();
     if($_SESSION["isLogin"]) {
         echo $_SESSION["id"];
